40757f006ba37b22e790bc98692c8c7a099817ad
mcousillas1 <64102996+mcousillas1@users.noreply.github.com>
2022-07-10 17:41:43 -0300

    Components de examen y materia

---
+++ src/components/ListSchedules.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native'
+
+export default function ListSchedules(name, teacher, year, schedule) {
+
+    return (
+        <View>
+            <View style={styles.modal}>
+                <Text style={styles.name}>
+                    {name}
+                </Text>
+                <Text style={styles.teacher}>
+                    {teacher}
+                </Text>
+                <Text style={styles.year}>
+                    {year}
+                </Text>
+                <Text style={styles.schedule}>
+                    {schedule}
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    modal: {
+        margin: 20,
+        backgroundColor: '#B4BAD9',
+        borderRadius: 10
+    },
+    name: {
+        margin: 12,
+        color: '#fff',
+        fontSize: 25,
+        fontWeight: 700
+    },
+    teacher: {
+        marginHorizontal: 12,
+        marginVertical: 5
+    },
+    year: {
+        marginHorizontal: 12,
+        marginVertical: 5
+    },
+    schedule: {
+        marginHorizontal: 12,
+        marginVertical: 5
+    }
+})

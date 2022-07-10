3e5b9657576731eb4ff0b7d8b17984d4ed98bdae
pgarcia4 <pgarcia4@correo.um.edu.uy>
2022-07-09 22:39:34 -0300

    First pages

---
+++ src/navigation/StackNavigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Main from '../pages/Main'
+import LogIn from '../pages/LogIn'
+
+const Stack = createNativeStackNavigator()
+
+export default function StackNavigation() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name='Main' component={Main}/>
+            <Stack.Screen name='LogIn' component={LogIn}/>
+        </Stack.Navigator>
+    )
+}

023222214e351650e04c50dba0d99b40b2bb17dd
pgarcia4 <pgarcia4@correo.um.edu.uy>
2022-07-10 10:51:33 -0300

    Main page

--- src/navigation/StackNavigation.jsx
+++ src/navigation/StackNavigation.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import Main from '../pages/Main'
-import LogIn from '../pages/LogIn'
+// import LogIn from '../pages/LogIn'
 
 const Stack = createNativeStackNavigator()
 
 export default function StackNavigation() {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+            headerShown: false
+        }}>
             <Stack.Screen name='Main' component={Main}/>
-            <Stack.Screen name='LogIn' component={LogIn}/>
+            {/* <Stack.Screen name='LogIn' component={LogIn}/> */}
         </Stack.Navigator>
     )
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,5 +2,85 @@ import React from 'react'
 import {
     View,
     Text,
-    Button
+    Image,
+    TouchableOpacity,
+    StyleSheet
 } from 'react-native'
+import { Button } from 'react-native-paper'
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function Main() {
+    return (
+        <View>
+            <Image source={require('../../assets/logo.png')} style={styles.logo}/>
+            <Text style={styles.title}>Menu Principal</Text>
+            <View style={styles.containerColumn}>
+                <TouchableOpacity style={styles.button}>
+                    <View style={styles.insideButton}>
+                        <Ionicons size={54} name="grid" color="white" />
+                        <Text style={styles.buttonText}>Grilla</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <View style={styles.insideButton}>
+                        <Ionicons size={54} name="calendar" color="white" />
+                        <Text style={styles.buttonText}>Fechas de examen</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <View style={styles.insideButton}>
+                        <Ionicons size={54} name="list" color="white" />
+                        <Text style={styles.buttonText}>Reclamos</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <View style={styles.insideButton}>
+                        <Ionicons size={54} name="time" color="white" />
+                        <Text style={styles.buttonText}>Horarios</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        marginVertical: 10,
+        marginHorizontal: 6,
+        backgroundColor: '#6380D5',
+        height: 140,
+        width: 140,
+        borderRadius: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    insideButton: {
+        alignItems: 'center'
+    },
+    buttonText: {
+        fontSize: 25,
+        color: '#ffffff',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    containerColumn: {
+        flex: 1,
+        flexWrap: "wrap",
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row'
+    },
+    logo: {
+        width: 145,
+        height: 65,
+        marginLeft:6
+    },
+    title: {
+        color: '#6380D5',
+        fontWeight: 700,
+        fontSize: 72,
+        textAlign: 'center',
+        marginVertical: 12
+    }
+})
